94cd9f94b97798b7789c18345e1d0590bb2f4225
robabg <robabogale00@gmail.com>
2024-12-18 12:24:44 +0300

    changes

--- HospitalManagmentSystem/HospitalManagmentSystem/LogIn.Designer.cs
+++ HospitalManagmentSystem/HospitalManagmentSystem/LogIn.Designer.cs
@@ -44,9 +44,10 @@ namespace HospitalManagmentSystem
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(114, 61);
+            label1.Location = new Point(163, 102);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(379, 34);
+            label1.Size = new Size(573, 51);
             label1.TabIndex = 0;
             label1.Text = "Welcome To HMS Hospital";
             // 
@@ -54,9 +55,10 @@ namespace HospitalManagmentSystem
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(107, 155);
+            label2.Location = new Point(153, 258);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(140, 24);
+            label2.Size = new Size(215, 37);
             label2.TabIndex = 1;
             label2.Text = "USER NAME:";
             // 
@@ -64,35 +66,39 @@ namespace HospitalManagmentSystem
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(107, 217);
+            label3.Location = new Point(153, 362);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(116, 24);
+            label3.Size = new Size(177, 37);
             label3.TabIndex = 2;
             label3.Text = "Password:";
             // 
             // txt_user
             // 
-            txt_user.Location = new Point(268, 156);
+            txt_user.Location = new Point(383, 260);
+            txt_user.Margin = new Padding(4, 5, 4, 5);
             txt_user.MaxLength = 25;
             txt_user.Name = "txt_user";
-            txt_user.Size = new Size(225, 23);
+            txt_user.Size = new Size(320, 31);
             txt_user.TabIndex = 3;
             // 
             // txt_password
             // 
-            txt_password.Location = new Point(268, 217);
+            txt_password.Location = new Point(383, 362);
+            txt_password.Margin = new Padding(4, 5, 4, 5);
             txt_password.Name = "txt_password";
             txt_password.PasswordChar = '*';
-            txt_password.Size = new Size(225, 23);
+            txt_password.Size = new Size(320, 31);
             txt_password.TabIndex = 4;
             txt_password.TextChanged += txt_password_TextChanged;
             // 
             // btn_login
             // 
             btn_login.BackColor = SystemColors.Control;
-            btn_login.Location = new Point(88, 297);
+            btn_login.Location = new Point(126, 495);
+            btn_login.Margin = new Padding(4, 5, 4, 5);
             btn_login.Name = "btn_login";
-            btn_login.Size = new Size(126, 32);
+            btn_login.Size = new Size(180, 53);
             btn_login.TabIndex = 5;
             btn_login.Text = "Log In";
             btn_login.UseVisualStyleBackColor = false;
@@ -100,9 +106,10 @@ namespace HospitalManagmentSystem
             // 
             // btn_clearLogin
             // 
-            btn_clearLogin.Location = new Point(367, 297);
+            btn_clearLogin.Location = new Point(524, 495);
+            btn_clearLogin.Margin = new Padding(4, 5, 4, 5);
             btn_clearLogin.Name = "btn_clearLogin";
-            btn_clearLogin.Size = new Size(126, 32);
+            btn_clearLogin.Size = new Size(180, 53);
             btn_clearLogin.TabIndex = 6;
             btn_clearLogin.Text = "Clear";
             btn_clearLogin.UseVisualStyleBackColor = true;
@@ -111,17 +118,18 @@ namespace HospitalManagmentSystem
             // dateTimePicker1
             // 
             dateTimePicker1.Enabled = false;
-            dateTimePicker1.Location = new Point(447, 12);
+            dateTimePicker1.Location = new Point(639, 20);
+            dateTimePicker1.Margin = new Padding(4, 5, 4, 5);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.Size = new Size(284, 31);
             dateTimePicker1.TabIndex = 7;
             // 
             // LoginPage
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ControlDark;
-            ClientSize = new Size(659, 436);
+            BackColor = Color.White;
+            ClientSize = new Size(941, 727);
             Controls.Add(dateTimePicker1);
             Controls.Add(btn_clearLogin);
             Controls.Add(btn_login);
@@ -130,6 +138,7 @@ namespace HospitalManagmentSystem
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "LoginPage";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "LogIn";

db4b6ae61518516c274185e48d62d4fddf30f5b6
Opshadow <brookteklebrhan123@gmail.com>
2024-12-19 09:49:29 +0300

    updated

--- HospitalManagmentSystem/HospitalManagmentSystem/Program.cs
+++ HospitalManagmentSystem/HospitalManagmentSystem/Program.cs
@@ -13,8 +13,8 @@ namespace HospitalManagmentSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new LoginPage());
-            Application.Run(new DoctorsPage());
+            Application.Run(new LoginPage());
+            // Application.Run(new DoctorsPage());
 
         }
     }
